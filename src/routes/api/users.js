3e38297947dac030ab1b6b6a718819931eda8858
shalu <chandwani.shalu@andela.com>
2020-02-21 15:42:20 +0200

    This is the chore repository for any backend project
    - has all the necessary chore setups
    - covering all the badges.

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -45,30 +45,30 @@ router.put("/user", function(req, res, next) {
         .catch(next);
 });
 
-router.post("/users/login", function(req, res, next) {
-    if (!req.body.user.email) {
-        return res.status(422).json({ errors: { email: "can't be blank" } });
-    }
+// router.post("/users/login", function(req, res, next) {
+//     if (!req.body.user.email) {
+//         return res.status(422).json({ errors: { email: "can't be blank" } });
+//     }
 
-    if (!req.body.user.password) {
-        return res.status(422).json({ errors: { password: "can't be blank" } });
-    }
-    passport.authenticate("local", { session: false }, function(
-        err,
-        user,
-        info
-    ) {
-        if (err) {
-            return next(err);
-        }
+//     if (!req.body.user.password) {
+//         return res.status(422).json({ errors: { password: "can't be blank" } });
+//     }
+//     passport.authenticate("local", { session: false }, function(
+//         err,
+//         user,
+//         info
+//     ) {
+//         if (err) {
+//             return next(err);
+//         }
 
-        if (user) {
-            return res.json({ user: user.toAuthJSON() });
-        } else {
-            return res.status(422).json(info);
-        }
-    })(req, res, next);
-});
+//         if (user) {
+//             return res.json({ user: user.toAuthJSON() });
+//         } else {
+//             return res.status(422).json(info);
+//         }
+//     })(req, res, next);
+// });
 
 router.post("/users", function(req, res, next) {
     const user = new User();
